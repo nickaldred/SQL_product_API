e99735c4eed1f25b4c10856af5aa55a19fb83af9
Nick Aldred <nickaldred@hotmail.co.uk>
2022-07-03 10:10:37 +0100

    Refactored flask app into as class

--- mainApp.py
+++ mainApp.py
@@ -12,189 +12,236 @@ from dotenv import load_dotenv
 import os
 from decorators import token_required
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    """
-    API landing page.
-
-    """
-    return jsonify({'message' : """Welcome to the purchasing software API. 
-        ----------- For list of commands please see /documentation"""})
 
 
-@app.route('/documentation')
-def documentation():
-    """
-    Displays the documentation on how to use the API.
+class APIApp:
 
-    """
-    workingdir = os.path.abspath(os.getcwd())
-    filepath = workingdir + '/static/files'
-    return send_from_directory(filepath, 'documentation.txt')
+    def __init__(self) -> None:
+        self.app = Flask(__name__)
 
-
-@app.route('/products')
-@token_required
-def get_all_products() -> json:
-    """
-    Returns all the products in the SQL database in JSON format.
-
-    """
-
-    output = []
-    products = database.find_all_products(cursor)
-
-    for product in products:
-            product_data = {'code' : product[0], 'name': product[1], 
-            'supplier' : product[2], 'cost' : product[3], 'rrp' : product[4],
-            'pcsl' : product[5], 'prsl' : product[6]}
-            output.append(product_data)
-
-    return (jsonify(output))
+        #Create url rules for API
+        self.app.add_url_rule('/', 'index', self.index)
+        self.app.add_url_rule(
+            '/documentation', 'documentation', self.documentation)
+        self.app.add_url_rule(
+            '/products', 'products', self.get_all_products)
+        self.app.add_url_rule(
+            '/product/<code>', 'product', self.get_product)
+        self.app.add_url_rule(
+            '/products/<supplier_name>', 'supplier_products', 
+            self.get_supplier_products)
+        self.app.add_url_rule(
+            '/products', 'add_products', self.add_product, methods=["POST"])
+        self.app.add_url_rule(
+            '/products/<code>', 'delete_product', self.delete_product, 
+            methods=['DELETE'])
+        self.app.add_url_rule(
+            '/product/<code>', 'update_product', self.update_product, 
+            methods=['PUT'])
+        self.app.add_url_rule(
+            '/login/<username>/<password>', 'login', self.login)
 
 
-@app.route("/product/<code>")
-#@token_required
-def get_product(code) -> json:
-    """
-    Returns a singular product using the product code.
+        #Rate limiter for API
+        self.limiter = Limiter(
+            self.app,
+            key_func=get_remote_address,
+            default_limits=["10000 per day", "500 per hour"]
+        )
 
-    Input:
-    code -> String - Code of product to be found.
-
-    """
-
-    product = database.find_product(cursor, code)
-
-    #Error checking
-    if product == []:
-        return jsonify({"Product not found" : 404})
-
-    product_data = {'code' : product[0][0], 'name': product[0][1], 
-            'supplier' : product[0][2], 'cost' : product[0][3], 
-            'rrp' : product[0][4],
-            'pcsl' : product[0][5], 'prsl' : product[0][6]}
-
-    return (jsonify([product_data]))
-
-
-@app.route("/products/<supplier_name>")
-#@token_required
-def get_supplier_products(supplier_name) -> json:
-    """
-    Returns all products from a particular supplier.
-
-    Input:
-    supplier_name -> String - Supplier to be searched for.
-    
-    """
-    #Converts underscores to space's
-    supplier_name = supplier_name.replace("_", " ")
-
-    output = []
-    products = database.find_suppliers_products(cursor, supplier_name)
-
-    for product in products:
-            product_data = {'code' : product[0], 'name': product[1], 
-            'supplier' : product[2], 'cost' : product[3], 'rrp' : product[4],
-            'pcsl' : product[5], 'prsl' : product[6]}
-            output.append(product_data)
-    
-    if output == []:
-        return jsonify({"Supplier not found": 404})
-
-    return jsonify(output)
+        #Connects to the Postgres self.database
+        self.database = Db(host="localhost",user=os.environ.get("DB_USER"), 
+            password=os.environ.get("DB_PASS"))
+        self.conn = self.database.connect_db()
+        self.cursor = self.database.create_cursor(self.conn)
+        table = """CREATE TABLE IF NOT EXISTS products
+                (productCode VARCHAR(32) PRIMARY KEY, 
+                productName VARCHAR(50),
+                productSupplier VARCHAR(50),  
+                productCost DECIMAL(19,2),
+                productRRP DECIMAL(19,2),
+                PCSL SMALLINT,
+                PRSL SMALLINT
+                )"""
+        self.database.create_db(self.cursor, "postgres", table)
 
 
-@app.route("/products", methods=["POST"])
-#@token_required
-def add_product():
-    """
-    Using the provided JSON data, adds a new product to the SQL 
-    database.
+    def index(self):
+        """
+        API landing page.
 
-    Input:
-    JSON data of new product.
-    
-    """
-
-    #Gathers data from request.
-    product = Product(code=request.json['code'], name=request.json['name'],
-    supplier=request.json['supplier'], cost=request.json['cost'], 
-    rrp=request.json['rrp'], pcsl=request.json['pcsl'], 
-    prsl=request.json['prsl'])
-
-    database.add_data(cursor, product)
-    database.commit_data(conn)
-    return jsonify({"Added product" : product.code})
+        """
+        return jsonify({'message' : """Welcome to the purchasing software API. 
+            ----------- For list of commands please see /documentation"""})
 
 
-@app.route("/product/<code>", methods=['DELETE'])
-#@token_required
-def delete_product(code):
-    """
-    Using a provided product code, deletes that product from the 
-    SQL database.
+    def documentation(self):
+        """
+        Displays the documentation on how to use the API.
 
-    Input:
-    code -> String - Code of product to be deleted.
-
-    """
-
-    database.delete_product(cursor, code)
-    database.commit_data(conn)
-    return jsonify({'message' : 'Deleted product'})
+        """
+        workingdir = os.path.abspath(os.getcwd())
+        filepath = workingdir + '/static/files'
+        return send_from_directory(filepath, 'documentation.txt')
 
 
-@app.route("/product/<code>", methods=['PUT'])
-def update_product(code):
-    """
-    Updates any column of the table with the provided input data.
+    @token_required
+    def get_all_products(self) -> json:
+        """
+        Returns all the products in the SQL self.database in JSON 
+        format.
 
-    Inputs:
-    code - String - Product to update.
+        """
 
-    column - String - column to update.
-    value - Data type varies depending on column - Value to update
-            column with.
-    
-    """
-    
-    column = request.json['column']
-    data_to_update = request.json['value']
+        output = []
+        products = self.database.find_all_products(self.cursor)
 
-    database.update_product(cursor, code, column, data_to_update)
-    database.commit_data(conn=conn)
+        for product in products:
+                product_data = {'code' : product[0], 'name': product[1], 
+                'supplier' : product[2], 'cost' : product[3], 
+                'rrp' : product[4],'pcsl' : product[5], 'prsl' : product[6]}
+                output.append(product_data)
 
-    return(get_product(code))
+        return (jsonify(output))
 
 
+    #@token_required
+    def get_product(self, code) -> json:
+        """
+        Returns a singular product using the product code.
 
-@app.route('/login/<username>/<password>')
-def login(username, password):
-    """
-    Creates a token after a succesful login so the API can be accessed.
+        Input:
+        code -> String - Code of product to be found.
 
-    Input:
-    Username - String 
-    Password - String
-    
-    """
+        """
 
-    if (username and password == os.environ.get("PASSWORD") and 
-            os.environ.get("USER_NAME") == username):
+        product = self.database.find_product(self.cursor, code)
 
-        #Creates a token with 120 mins to expire.
-        token = jwt.encode({'user' : username, 
-        'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=120)},
-         os.environ.get("SECRET_KEY"))
-         
-        return jsonify({'token' : token})
+        #Error checking
+        if product == []:
+            return jsonify({"Product not found" : 404})
 
-    return make_response('Could not verify!', 401, 
-        {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+        product_data = {'code' : product[0][0], 'name': product[0][1], 
+                'supplier' : product[0][2], 'cost' : product[0][3], 
+                'rrp' : product[0][4],
+                'pcsl' : product[0][5], 'prsl' : product[0][6]}
+
+        return (jsonify([product_data]))
+
+
+    #@token_required
+    def get_supplier_products(self, supplier_name) -> json:
+        """
+        Returns all products from a particular supplier.
+
+        Input:
+        supplier_name -> String - Supplier to be searched for.
+        
+        """
+        #Converts underscores to space's
+        supplier_name = supplier_name.replace("_", " ")
+
+        output = []
+        products = self.database.find_suppliers_products(self.cursor, 
+            supplier_name)
+
+        for product in products:
+                product_data = {'code' : product[0], 'name': product[1], 
+                'supplier' : product[2], 'cost' : product[3], 
+                'rrp' : product[4], 'pcsl' : product[5], 'prsl' : product[6]}
+                output.append(product_data)
+        
+        if output == []:
+            return jsonify({"Supplier not found": 404})
+
+        return jsonify(output)
+
+
+    #@token_required
+    def add_product(self):
+        """
+        Using the provided JSON data, adds a new product to the SQL 
+        self.database.
+
+        Input:
+        JSON data of new product.
+        
+        """
+
+        #Gathers data from request.
+        product = Product(code=request.json['code'], 
+        name=request.json['name'], supplier=request.json['supplier'], 
+        cost=request.json['cost'], rrp=request.json['rrp'], 
+        pcsl=request.json['pcsl'], prsl=request.json['prsl'])
+
+        self.database.add_data(self.cursor, product)
+        self.database.commit_data(self.conn)
+        return jsonify({"Added product" : product.code})
+
+
+    #@token_required
+    def delete_product(self, code):
+        """
+        Using a provided product code, deletes that product from the 
+        SQL self.database.
+
+        Input:
+        code -> String - Code of product to be deleted.
+
+        """
+
+        self.database.delete_product(self.cursor, code)
+        self.database.commit_data(self.conn)
+        return jsonify({'message' : 'Deleted product'})
+
+
+    def update_product(self, code):
+        """
+        Updates any column of the table with the provided input data.
+
+        Inputs:
+        code - String - Product to update.
+
+        column - String - column to update.
+        value - Data type varies depending on column - Value to update
+                column with.
+        
+        """
+        
+        column = request.json['column']
+        data_to_update = request.json['value']
+
+        self.database.update_product(self.cursor, code, 
+            column, data_to_update)
+        self.database.commit_data(conn=self.conn)
+
+        return(self.get_product(code))
+
+
+    def login(self, username, password):
+        """
+        Creates a token after a succesful login so the API can be 
+        accessed.
+
+        Input:
+        Username - String 
+        Password - String
+        
+        """
+
+        if (username and password == os.environ.get("PASSWORD") and 
+                os.environ.get("USER_NAME") == username):
+
+            #Creates a token with 120 mins to expire.
+            token = jwt.encode({'user' : username, 
+            'exp' : datetime.datetime.utcnow() + 
+                datetime.timedelta(minutes=120)},
+            os.environ.get("SECRET_KEY"))
+            
+            return jsonify({'token' : token})
+
+        return make_response('Could not verify!', 401, 
+            {'WWW-Authenticate' : 'Basic realm="Login Required"'})
 
 
 
@@ -204,28 +251,6 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    #Connects to the Postgres database
-    database = Db(host="localhost",user=os.environ.get("DB_USER"), 
-        password=os.environ.get("DB_PASS"))
-    conn = database.connect_db()
-    cursor = database.create_cursor(conn)
-    table = """CREATE TABLE IF NOT EXISTS products
-            (productCode VARCHAR(32) PRIMARY KEY, 
-            productName VARCHAR(50),
-            productSupplier VARCHAR(50),  
-            productCost DECIMAL(19,2),
-            productRRP DECIMAL(19,2),
-            PCSL SMALLINT,
-            PRSL SMALLINT
-            )"""
-    database.create_db(cursor, "postgres", table)
-
-
-    #Rate limiter for API
-    limiter = Limiter(
-        app,
-        key_func=get_remote_address,
-        default_limits=["10000 per day", "500 per hour"]
-    )
+    apiApp = APIApp()
         
-    app.run(host="127.0.0.2", debug=True)
+    apiApp.app.run(host="127.0.0.2", debug=True)
